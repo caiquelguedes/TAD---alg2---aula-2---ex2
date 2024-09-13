8e1a87b65efaa4152a6af5eac653e47bdcb0ee70
caiquelguedes <caiqueluizghpo@gmail.com>
2024-09-13 17:37:12 -0300

    Create compiler.cpp

---
+++ compiler.cpp
@@ -0,0 +1,65 @@
+#include <iomanip>
+using namespace std;
+
+class Aluno
+{
+private:
+    int matricula;
+    int idade;
+    double notas[3];
+public:
+    void definirAluno();
+    void imprimirAluno();
+    double calcularMedia();
+    bool foiAprovado();
+};
+void Aluno::definirAluno()
+{
+    cout << "Informe a porra da matricula, filho da puta: ";
+    cin >> matricula;
+    cout << "Informe a idade do lolzero: ";
+    cin >> idade;
+    cout << "Informe a nota do idiota: ";
+    for(int i = 0; i < 3; i++)
+    {
+        cin >> notas[i];
+    }
+
+}
+void Aluno::imprimirAluno()
+{
+    cout << "Matricula: " << matricula << endl;
+    cout << "Idade: " << matricula << endl;
+    cout << "Notas: " << matricula << endl;
+    for(int i = 0; i < 3; i++)
+    {
+        cout << notas[i] << " ";
+    }
+}
+double Aluno::calcularMedia()
+{
+    int soma = notas[0];
+    for(int i = 1; i < 3; i++)
+    {
+        soma += notas[i];
+    }
+    return soma/3.0;
+}
+bool Aluno::foiAprovado()
+{
+
+    if(calcularMedia() >= 6.0)
+    {
+        cout << "\nAprovado!";
+             return true;
+    }
+    else
+    {
+        cout << "\nReprovado!";
+        return false;
+    }
+
+}
+
+
+
